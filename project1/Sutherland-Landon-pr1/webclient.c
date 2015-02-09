c6ab342585ea664d5955180d790f9098ac802fda
sutherlandon <sutherlandon@gmail.com>
2015-02-09 00:08:44 -0500

    working on FTP, getting closer

--- project1/Sutherland-Landon-pr1/webclient.c
+++ project1/Sutherland-Landon-pr1/webclient.c
@@ -19,10 +19,65 @@ void error( const char* msg )
 	exit(0);
 }
 
+// takes a string and replaces \n with \0
+void stripNewline( char *line )
+{
+	while( *line != 0 )
+	{
+		if( *line == '\n' )
+			*line = '\0';
+		line++;
+	}
+}
+
+/**
+ * Takes a server response and parses it
+ * @param const char* respose
+ *		A string response from a server request
+ * @return
+ *		A code based on what was parsed
+ *		code: -1, parse error, server response was incorrectly formatted
+ *		code: 0, NO_FILE_FOUND, the requested file was not found
+ *		code: > 0, the index in the respose to begin reading the data
+ */
+int readResponse( const char* response, char** file_bytes, int file_size )
+{
+	int code = -1;
+	char *token, *rest = strdup( response );
+
+	// check the first token (GetFile)
+	if(( token = strsep( &rest, " " )) != NULL )
+		if( strcmp( token, "GetFile" ) == 0 ) 
+		{
+			// check the second token (GET)
+			if(( token = strsep( &rest, " " )) != NULL )
+			{
+				if( strcmp( token, "OK" ) == 0 )
+				{
+	printf( "saving file size\n" );
+	fflush( stdout );
+					file_size = atoi( strsep( &rest, " " ));
+	printf( "%d bytes - saving file bytes\n", file_size );
+	printf( "%lu, %lu\n", sizeof( *file_bytes ), sizeof( strdup(rest) ));
+	fflush( stdout );
+					strcpy( *file_bytes, rest );
+	printf( "saved bytes: %s\n", *file_bytes );
+	fflush( stdout );
+					code = 1;
+				} else {
+					if( strcmp( token, "FILE_NOT_FOUND" ) == 0 )
+						code = 0;
+				}
+			}
+		}
+
+	return code;
+}
+
 int main(int argc, char **argv)
 {
 	// client settings
-	int client_sockfd, bytes;
+	int client_sockfd, bytes, total_bytes;
 	struct sockaddr_in server_address;
 	struct hostent *server;
 	char *buffer = (char *) malloc( 256 );
@@ -33,12 +88,16 @@ int main(int argc, char **argv)
 	int port = 8888;
 	int thread_count = 1;
 	int requests = 10;
-	char *server_host="0.0.0.0";
+	char *server_host = "0.0.0.0";
 	char *workload_path = "workload.txt";
 	FILE *workload;
-	char *metrics_path="metrics.txt";
+	char *metrics_path = "metrics.txt";
 	FILE *metrics;
-	char *download_path=NULL;
+	char *download_path = NULL;
+
+	// server response
+	char *file_bytes = (char *) malloc( 256 );
+	int file_size, response_code;
 	
 	// get arguments from the command line
 	int i;
@@ -96,17 +155,15 @@ int main(int argc, char **argv)
 	printf( "opening workload: %s\n", workload_path );
 	workload = fopen( workload_path, "r" );
 	if( workload == NULL )
-		error( "could not open workload file\n" );
+		error( "ERROR could not open workload file\n" );
 
-	printf( "reading...\n" );
 	// read the file and request the files
 	while(( bytes = getline( &line, &len, workload )) != -1 )
 	{
 		// create the socket
-		printf( "creating socket...\n" );
 		client_sockfd = socket( AF_INET, SOCK_STREAM, 0 );
 		if( client_sockfd == SOCKET_ERROR )
-			error( "Could not create socket\n" );
+			error( "ERROR could not create socket\n" );
 
 		// connect to the server
 		printf( "connecting to the server...\n" );
@@ -114,23 +171,45 @@ int main(int argc, char **argv)
 			error( "ERROR could not connect to server\n" );
 	
 		// get the message to send
-		//bzero( buffer, 256 );
-		strcpy( buffer, "getFile GET " );
-		strcat( buffer, line );
+		bzero( buffer, 256 ); // zero out the buffer
+		stripNewline( line );
+		snprintf( buffer, 255, "GetFile GET %s", line );
 		printf( "%s\n", buffer );
 
-		// write the message
-		bytes = write( client_sockfd, buffer, strlen( buffer ));
+		// send the file request
+		bytes = send( client_sockfd, buffer, strlen( buffer ), 0 );
 		if( bytes < 0 )
 			error( "ERROR writing to socket\n" );
 		printf( "%d bytes were sent\n", bytes );
 		
-		bzero( buffer, 256 );
-		bytes = read( client_sockfd, buffer, 255 );
-		if( bytes < 0 )
-			error( "ERROR reading from socket\n" );
-		printf( "%s\n", buffer );
-		printf( "%d bytes were received\n", bytes );
+		// receive the server response
+		bzero( buffer, 256 ); // zero out the buffer
+		bytes = recv( client_sockfd, buffer, 255, 0 );
+	printf( "received %d bytes\n", bytes );
+	fflush( stdout );
+		response_code = readResponse( buffer, &file_bytes, file_size );
+	printf( "parsed response" );
+	fflush( stdout );
+		if( response_code == 1 )
+		{
+			printf( "%s (%d bytes)\n", file_bytes, file_size );
+			// process incoming file
+			while(( bytes = recv( client_sockfd, buffer, 255, 0 )) > 0 )
+			{
+				total_bytes += bytes;
+				//printf( "received: %d bytes\n", bytes );
+				bzero( buffer, 256 ); // zero out the buffer
+			}
+			printf( "received: %d bytes\n", total_bytes );
+		}
+		else if( response_code == 0 )
+		{
+			// print no file found
+			printf( "Server could not find the specified file (FILE_NOT_FOUND)\n" );
+		} else {
+			// code=-1, print formatting error
+			printf( "ERROR incorrect server respose format, you may be under attack!\n" );
+		}
 
 		// close the connection to the server
 		printf( "closing the socket. bytes: %d\n", bytes );
--- project1/Sutherland-Landon-pr1/webserver.c
+++ project1/Sutherland-Landon-pr1/webserver.c
@@ -17,6 +17,45 @@ void error( const char* msg )
 	exit(0);
 }
 
+// given and int, returns how many digits are in it
+int intlen( int i )
+{
+	int n = 0;
+	while( i ) { i /= 10; n++; }
+	return n;
+}
+
+/**
+ * Get the name of a file to server from a request
+ * @param const char* request
+ *		Format: GetFile GET <filename>
+ * @return
+ *		char* <filname>
+ *		NULL if there was an error in the format
+ */
+char *getFileName( const char* request )
+{
+	char *file_name = NULL;
+	char *token, *rest = strdup(request);
+	
+	// check the first token (GetFile)
+	if(( token = strsep( &rest, " " )) != NULL )
+		if( strcmp( token, "GetFile" ) == 0 )
+		{
+			// check the second token (GET)
+			if(( token = strsep( &rest, " " )) != NULL )
+				if( strcmp( token, "GET" ) == 0 )
+				{
+					// take the third token as the file name
+					if(( token = strsep( &rest, " " )) != NULL )
+						file_name = strdup( token );
+				}
+		}
+
+	// return the file_name, or null if none was found
+	return file_name;
+}
+
 int main(int argc, char **argv)
 {
 	// initialize the server
@@ -24,12 +63,22 @@ int main(int argc, char **argv)
 	int server_sockfd, client_sockfd;
 	struct sockaddr_in server_address, client_address;
 	socklen_t client_size = sizeof( client_address );
-	char buffer[256];
 
 	// user input defaults
-	char* path=".";
+	char *path_to_files = ".";
 	int port = 8888;
 	int thread_count = 1;
+
+	// serving variables
+	char *buffer = (char *) malloc( 256 );
+	char *reply = (char *) malloc( 256 );
+	char *get_file_ok = "GetFile OK";
+	char *get_file_not_found = "GetFile FILE_NOT_FOUND 0 0";
+	char *file_name = (char *) malloc( 256 );
+	int file_size = 0;
+	FILE *file_to_serve;
+	int last_read = 1, reply_size = 0;
+	int bytes_read = 0, bytes = 0;
 	
 	// get arguments from the command line
 	int i;
@@ -42,55 +91,121 @@ int main(int argc, char **argv)
 			thread_count = atoi(argv[++i]);
 
 		if( strcmp( argv[i], "-f" ) == 0 )
-			path = argv[++i];
+			path_to_files = argv[++i];
 
 		if( strcmp( argv[i], "-h" ) == 0 )
 		{
-			printf( "usage:\n\twebserver [options]\noptions:\n\t-p port (Default: 8888)\n\t-t number of worker threads (Default: 1, Range: 1-1000)\n\t-f pather to static files (Default: .)\n\t-h show help message\n" );
+			printf( "usage:\n\twebserver [options]\noptions:\n\t-p port (Default: 8888)\n\t-t number of worker threads (Default: 1, Range: 1-1000)\n\t-f path to static files (Default: .)\n\t-h show help message\n" );
 			return 0;
 		}
 	}
-	printf("port %d, threads %d, path %s\n", port, thread_count, path);
 	printf( "initialzing server...\n" );
+	printf( "port %d, threads %d, path %s\n", port, thread_count, path_to_files );
 
 	// create the socket
-	printf("making socket...\n");
+	printf( "making socket...\n" );
 	server_sockfd = socket( AF_INET, SOCK_STREAM, 0 );
 	if( server_sockfd == SOCKET_ERROR )
-		error("ERROR opening socket\n");
+		error( "ERROR opening socket\n" );
 
 	// connect to the host
-	printf("connecting to host on port %d...\n", port);
+	printf( "connecting to host on port %d...\n", port );
 	server_address.sin_family = AF_INET;
 	server_address.sin_addr.s_addr = INADDR_ANY;
-	server_address.sin_port = htons(port);
+	server_address.sin_port = htons( port );
 	if( bind( server_sockfd, (struct sockaddr *) &server_address, sizeof( server_address )) == SOCKET_ERROR )
-		error("ERROR on binding");
+		error( "ERROR connecting to host" );
 
-	// create the queue for listening, up to 5 connections may be waiting to connect to the server at a time
-	printf("making listening queue...\n");
+	// create the queue for listening
+	// up to 5 connections may be waiting to connect to the server at a time
 	listen( server_sockfd, QUEUE_SIZE );
 
 	// listen for incoming clients and accept them
-	printf("listening...\n");
+	printf( "listening...\n" );
 	do {
 		client_sockfd = accept( server_sockfd, (struct sockaddr *) &client_address, &client_size );
 		if( client_sockfd == SOCKET_ERROR )
-			error("ERROR accepting client\n");
+			error( "ERROR accepting client\n" );
 		printf( "----------------------------------\n" );
 
-		printf("client connected\n");
+		printf( "client connected\n" );
 		bzero( buffer, 256); // zero out the buffer
 
-		// read the client's message
-		if( read( client_sockfd, buffer, 255 ) == SOCKET_ERROR )
+		// get the client request
+		if( recv( client_sockfd, buffer, 255, 0 ) == SOCKET_ERROR )
 			error( "ERROR reading from client socket\n" );
-		printf( "Client says: %s", buffer );
+		printf( "client request: %s\n", buffer );
 
-		// send a reply
-		if( write( client_sockfd, buffer, strlen( buffer )) == SOCKET_ERROR )
-			error( "ERROR writing to client socket\n" );
-		printf( "confirmation sent\n" );
+		// extract the file name, add it to the path, and open it
+		snprintf( file_name, 255, "%s%s", path_to_files, getFileName( buffer ));
+		printf( "fetching file: %s\n", file_name );
+		file_to_serve = fopen( file_name, "rb" );
+
+		if( file_to_serve != NULL )
+		{
+			// get the file size
+			fseek( file_to_serve, 0L, SEEK_END );
+			file_size = ftell( file_to_serve );
+			fseek( file_to_serve, 0L, SEEK_SET );
+			printf( "file size: %d\n", file_size );
+
+			// send the header and the first byte of the file
+			bzero( buffer, 256); // zero out the buffer
+			if(( bytes_read = fread( buffer, 1, 1, file_to_serve )) != 0 )
+			{
+				// format the reply
+				snprintf( reply, 255, "%s %d %s", get_file_ok, file_size, buffer );
+				reply_size = 2 + sizeof( get_file_ok ) +  intlen( file_size ) + bytes_read;
+				printf( "reply size: %d\n", reply_size );
+
+				// write each piece of the file to the socket
+				if(( bytes = send( client_sockfd, reply, reply_size, 0 )) == SOCKET_ERROR )
+					error( "ERROR writing to client socket\n" );
+				printf( "sent: %s (%d bytes)\n", reply, bytes );
+
+				bzero( buffer, 256); // zero out the buffer
+				bzero( reply, 256); // zero out the buffer
+			}
+
+			// send the rest of the file 128 bytes at a time
+			while(( bytes_read = fread( buffer, 1, 128, file_to_serve )) != 0 )
+			{
+				// write each piece of the file to the socket
+				if(( bytes = send( client_sockfd, buffer, bytes_read, 0 )) == SOCKET_ERROR )
+					error( "ERROR writing to client socket\n" );
+				printf( "sent: %d bytes\n", bytes );
+
+				bzero( buffer, 256); // zero out the buffer
+				bzero( reply, 256); // zero out the buffer
+			}
+
+			// reading in 128 byte chunks we might miss the last few
+			// if file_size % 128 != 0 So get the last few
+			i = 0;
+			bytes_read = 0;
+			while( fread( &buffer[i++], 1, 1, file_to_serve ) != 0 )
+				bytes_read++;
+
+			// send the last bit of the file
+			if( bytes_read > 0 )
+			{
+				// write the last piece of the file to the socket
+				if(( bytes = send( client_sockfd, buffer, bytes_read, 0 )) == SOCKET_ERROR )
+					error( "ERROR writing to client socket\n" );
+				printf( "sent: %d bytes\n", bytes );
+
+				bzero( buffer, 256); // zero out the buffer
+				bzero( reply, 256); // zero out the buffer
+			}
+
+			// close the file
+			fclose( file_to_serve );
+		} else {
+			// NO file found, send reply
+			if( send( client_sockfd, get_file_not_found, strlen( get_file_not_found ), 0 ) == SOCKET_ERROR )
+				error( "ERROR writing to client socket\n" );
+			printf( "GetFile FILE_NOT_FOUND reply sent\n" );
+		}
 
 		// close this client
 		close( client_sockfd );
