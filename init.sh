56c2977f976240b4c283cb64db93f94f49b5ffb1
Landon Sutherland <Sutherlandon@gmail.com>
2016-08-30 22:11:13 -0600

    Fixed the global config bug in the init script.  So now it should work!

--- init.sh
+++ init.sh
@@ -5,8 +5,8 @@ ln -s ~/mouse-hole/.bash_profile ~/.bash_profile
 ln -s ~/mouse-hole/.vimrc ~/.vimrc
 ln -s ~/mouse-hole/.vim ~/.vim
 
-git --global config user.name "Landon Sutherland"
-git --gloabl config user.email "Sutherlandon@gmail.com"
+git config --global user.name "Landon Sutherland"
+git config --global user.email "Sutherlandon@gmail.com"
 
 source .bash_profile
 echo "done"
